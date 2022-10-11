2942d6e97beb889e9608827c31ea727a05141d82
public1992 <livethereality22@gmail.com>
2022-10-11 16:40:10 +0900

    테스트-커밋

---
+++ springMerge/src/main/java/com/kh/springMerge/SpringMergeApplication.java
@@ -0,0 +1,13 @@
+package com.kh.springMerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMergeApplication.class, args);
+	}
+
+}
---
+++ springMerge/src/test/java/com/kh/springMerge/SpringMergeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.springMerge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMergeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
