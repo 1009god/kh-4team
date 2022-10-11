c6c4b317ea031239f5243dab2030186a3797e921
1009god3 <1009god3@naver.com>
2022-10-11 17:41:32 +0900

    충돌 해결해보기

--- springMerge/src/main/java/com/kh/springMerge/controller/TestController.java
+++ springMerge/src/main/java/com/kh/springMerge/controller/TestController.java
@@ -1,21 +1,20 @@
 package com.kh.springMerge.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/test")
 public class TestController {
 	
-	int a=10;
-	int b=20;
+	@GetMapping("/hwangtaehyeon")
+	@ResponseBody
+	public String hwangtaehyeon() {
+		return "hwangtaehyeon";
+	}
 	
-	@Controller
-	@RequestMapping("/test")
-	public class TestController {
-		
-		int a=10;
-		int b=20;
 		
 		
 	
