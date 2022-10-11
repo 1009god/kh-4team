0abdf73d159f80d60bdc67f7c510c962b6a4963b
1009god3 <1009god3@naver.com>
2022-10-11 17:41:32 +0900

    충돌 해결해보기

--- springMerge/src/main/java/com/kh/springMerge/controller/TestController.java
+++ springMerge/src/main/java/com/kh/springMerge/controller/TestController.java
@@ -1,23 +1,21 @@
 package com.kh.springMerge.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/test")
 public class TestController {
 	
+
 	//최지안-병합테스트
 	//src 폴더에 .keep파일 추가
 	int a=10;
 	int b=20;
+
 	
-	@Controller
-	@RequestMapping("/test")
-	public class TestController {
-		
-		int a=10;
-		int b=20;
 		
 		
 	
