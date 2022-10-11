ec9b0886521d378111d369bb6a4ff72aaade2d8a
1009god3 <1009god3@naver.com>
2022-10-11 17:15:51 +0900

    다시 한번

--- springMerge/src/main/java/com/kh/springMerge/controller/TestController.java
+++ springMerge/src/main/java/com/kh/springMerge/controller/TestController.java
@@ -1,19 +1,14 @@
 package com.kh.springMerge.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/test")
 public class TestController {
 	
-	@GetMapping("/hwnagtaehyeon")
-	@ResponseBody
-	public String hwangtaehyeon() {
-		return "hwangtaehyeon";
-	}
+	int a=10;
+	int b=20;
 	
 		
 
