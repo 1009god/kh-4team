d1281125416d1968668bf9d83bd11b0b4afee7d7
public1992 <livethereality22@gmail.com>
2022-10-11 16:50:11 +0900

    머지 테스트 2

---
+++ springMerge/src/main/java/com/kh/springMerge/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.kh.springMerge.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+	
+	int a=10;
+	
+		
+
+}
