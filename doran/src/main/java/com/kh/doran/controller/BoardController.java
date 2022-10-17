903dcda55dec7e2897ba50b9df1bcf42d9dc059e
1009god <1009god3@navercom>
2022-10-18 02:22:11 +0900

    컨트롤러 테스트

---
+++ doran/src/main/java/com/kh/doran/controller/BoardController.java
@@ -0,0 +1,10 @@
+package com.kh.doran.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/board")
+public class BoardController {
+
+	String a ="황태현";
+}
