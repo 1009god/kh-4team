c2e3330359339b3c4fc40c92c107db5ecb2cfb4d
public1992 <livethereality22@gmail.com>
2022-10-20 14:58:07 +0900

    detail 에 회원이 좋아요 했는지 체크하는 기능 추가, LikesDaoImpl에 @Repository 어노테이션 빼먹은거 달았음

--- doran/src/main/java/com/kh/doran/controller/PjController.java
+++ doran/src/main/java/com/kh/doran/controller/PjController.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
+import com.kh.doran.constant.SessionConstant;
+import com.kh.doran.entity.LikesDto;
+import com.kh.doran.repository.LikesDao;
+import com.kh.doran.repository.MemDao;
 import com.kh.doran.repository.OptionsDao;
 import com.kh.doran.repository.PjDao;
 
@@ -17,17 +20,30 @@ import com.kh.doran.repository.PjDao;
 @RequestMapping("/pj")
 public class PjController {
 	
+	
+	
 	@Autowired
 	private PjDao pjDao;
 	
 	@Autowired
 	private OptionsDao optionsDao;
 	
+	@Autowired
+	private LikesDao likesDao;
+
+	
 	@GetMapping("/detail")
 	public String detail(@RequestParam int pjNo, Model model, HttpSession session) {
 		model.addAttribute("PjDto", pjDao.selectOne(pjNo));//프로젝트넘버로 검색해서 나온 값 model에 저장해서 넘김
 		model.addAttribute("OptionsDto", optionsDao.selectList(pjNo));//pjno로 검색해서 나온 옵션들 model에 저장해서 넘김
-		
+		String loginId=(String) session.getAttribute(SessionConstant.EMAIL);
+		//회원일 경우 좋아요 했는지 기록을 첨부
+		if(loginId!=null) {
+		LikesDto likesDto=new LikesDto();
+		likesDto.setLikesPjNo(pjNo);
+		likesDto.setLikesMemEmail(loginId);
+		model.addAttribute("isLike", likesDao.check(likesDto));
+		}
 		return "pj/detail";
 	};
 	
