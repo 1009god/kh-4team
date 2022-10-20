1e8a69f85278bc295a1fe9f57d3229ca822ab21c
scotty1003 <scotty1003@naver.com>
2022-10-20 14:43:24 +0900

    멤버 로그아웃 로그인 세션 포함 수정

--- doran/src/main/java/com/kh/doran/controller/MemController.java
+++ doran/src/main/java/com/kh/doran/controller/MemController.java
@@ -1,5 +1,7 @@
 package com.kh.doran.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,6 +36,34 @@ public class MemController {
 	@GetMapping("/login")
 	public String login() {
 		return "mem/login";
+	}
+	
+	@PostMapping("/login")
+	public String login(
+			@ModelAttribute MemDto inputDto,
+			HttpSession session) {
+		MemDto findDto = memDao.selectOne(inputDto.getMemEmail());
+		if(findDto == null) { //아이디 틀리면 로그인창
+			return "redirect:login?error"; 
+		}
+		
+		boolean passwordMatch = 
+				inputDto.getMemPw().equals(findDto.getMemPw());
+		if(passwordMatch) {
+			session.setAttribute("loginId", inputDto.getMemEmail());
+			
+			return "redirect:/"; //홈페이지로 보내주자 맞으면
+		}
+		else {
+			return "redirect:login?error"; //리다이렉트 = 겟, 비번 틀리면 로그인창
+		}
+		
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("loginId");
+		return "redirect:/";
 	}
 
 }
