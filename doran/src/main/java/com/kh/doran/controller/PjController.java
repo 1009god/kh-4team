878288fbfb5a8acb956d9f1eface744138a5d5fe
miniary <oooo8912@naver.com>
2022-10-20 12:09:27 +0900

    pjList 구현중

--- doran/src/main/java/com/kh/doran/controller/PjController.java
+++ doran/src/main/java/com/kh/doran/controller/PjController.java
@@ -7,14 +7,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.kh.doran.repository.PjDao;
+
 @Controller
 @RequestMapping("/pj")
 public class PjController {
+	
+	@Autowired
+	private PjDao pjDao;
 	
 //	@GetMapping("/detail")
 //	public String detail(@RequestParam int pjNo, Model model) {
 //		
 //	};
+	
+	@GetMapping("/list")
+	public String list(Model model, 
+			@RequestParam(required = false) String type,
+			@RequestParam(required = false) String keyword) {
+		boolean isSearch = type!=null && keyword!=null;
+		
+		if(isSearch) {
+			model.addAttribute("list",pjDao.selectList(type, keyword));
+		}
+		else {
+			model.addAttribute("list", pjDao.selectList());
+		}
+		return "pj/list";
+	};
 	
 
 }
