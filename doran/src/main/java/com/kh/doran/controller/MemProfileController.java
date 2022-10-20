4cf85af94191cfa7af78fc42a2af8d123913c948
gieun <gge0828@naver.com>
2022-10-20 11:15:56 +0900

    memDao , memDaoImpl에 selectOne기능 추가

--- doran/src/main/java/com/kh/doran/controller/MemProfileController.java
+++ doran/src/main/java/com/kh/doran/controller/MemProfileController.java
@@ -1,8 +1,34 @@
 package com.kh.doran.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.doran.entity.MemDto;
 
 @Controller
+@RequestMapping("/mypage")
 public class MemProfileController {
+	
+//	@GetMapping("/profile")
+//	public String mypage(HttpSession session, Model model) {
+//		//1. 세션에 들어있는 아이디를 꺼낸다 (down casting다운캐스팅) 형변환?
+//		//- 세션에 저장된 형태가 Object이기 때문에 string으로 다운캐스팅
+//		String memEmail = (String) session.getAttribute("memEmail");
+//		
+//		
+//		//2. 아이드를 이용하여 회원정보를 불러온다
+//		MemDto memDto = memDao.selectOne(memEmail);
+//		
+//		//3.불러온 정보를 모델에 첨부한다
+//		model.addAttribute("memDto", memDto);
+//		
+//		//4.화면(view)으로 전달(forward)한다
+//		
+//		return "mypage/profile";
+//	}
 	
 }
--- doran/src/main/java/com/kh/doran/repository/MemDaoImpl.java
+++ doran/src/main/java/com/kh/doran/repository/MemDaoImpl.java
@@ -1,7 +1,12 @@
 package com.kh.doran.repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Repository;
 
 import com.kh.doran.entity.MemDto;
@@ -23,6 +28,27 @@ public class MemDaoImpl implements MemDao {
 //			dto.setMemNick(rs.getString("memNick"));
 //		}
 //	} 미완
+	
+	private ResultSetExtractor<MemDto> extractor = new ResultSetExtractor<MemDto>() {
+		
+		@Override
+		public MemDto extractData(ResultSet rs) throws SQLException, DataAccessException {
+			if(rs.next()) {
+				MemDto dto = new MemDto();
+				dto.setMemNo(rs.getInt("mem_no"));
+				dto.setMemEmail(rs.getString("mem_email"));
+				dto.setMemPw(rs.getString("mem_pw"));
+				dto.setMemNick(rs.getString("mem_nick"));
+				dto.setMemTel(rs.getString("mem_tel"));
+				dto.setMemJoin(rs.getDate("mem_join"));
+				dto.setMemRoute(rs.getString("mem_route"));	
+				return dto;
+			}
+			else {
+				return null;
+			}
+		}
+	};
 			
 	public void insert(MemDto memDto) {
 		String sql = "insert into mem( "
@@ -37,7 +63,19 @@ public class MemDaoImpl implements MemDao {
 		};
 		jdbcTemplate.update(sql, param);
 	}
+
+	@Override
+	public MemDto selectOne(String memEmail) {
+		String sql = "select * from mem where mem_email = ?";
+		Object[] param = {memEmail};
+		return jdbcTemplate.query(sql, extractor, param);
+	}
+	
+	
 	
 	
 	
 }
+
+
+	
