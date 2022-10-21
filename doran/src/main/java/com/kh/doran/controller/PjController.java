3e7c20217c47ddcffae4d32ea0e2ad123afb9461
public1992 <livethereality22@gmail.com>
2022-10-21 12:06:35 +0900

    Merge branch 'jiahn_pj_detail' into main

--- doran/src/main/java/com/kh/doran/controller/PjController.java
+++ doran/src/main/java/com/kh/doran/controller/PjController.java
@@ -51,10 +51,10 @@ public class PjController {
 	public String detail(@RequestParam int pjNo, Model model, HttpSession session) {
 		model.addAttribute("PjDto", pjDao.selectOne(pjNo));//프로젝트넘버로 검색해서 나온 값 model에 저장해서 넘김
 		model.addAttribute("OptionsDto", optionsDao.selectList(pjNo));//pjno로 검색해서 나온 옵션들 model에 저장해서 넘김
-		String loginId=(String) session.getAttribute("loginId");
-		MemDto findDto=new MemDto();
-		findDto=memDao.selectOne(loginId);
-		int qwerty=findDto.getMemNo();
+//		String loginId=(String) session.getAttribute("loginId");
+//		MemDto findDto=new MemDto();
+//		findDto=memDao.selectOne(loginId);
+//		int qwerty=findDto.getMemNo();
 		
 		return "pj/detail";
 	};
