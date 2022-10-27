3dc0297f353b6ab6a3a7fcd9f2c2921044f1c81e
gieun <gge0828@naver.com>
2022-10-27 15:47:24 +0900

    마이페이지 프로필 사진 업로드 기능 추가
    아직 업로드된 사진을 불러와서 보여주는건 구현 안됐음

--- doran/src/main/java/com/kh/doran/repository/FilesDao.java
+++ doran/src/main/java/com/kh/doran/repository/FilesDao.java
@@ -10,4 +10,9 @@ public interface FilesDao {
 	List<FilesDto> selectList(); 
 	FilesDto selectOne(int filesNo);//프라이머리키로 조회
 	boolean delete(int filesNo); //프라이머리키로 삭제
+	
+	//mem프로필이미지 파일 저장
+	void connectFiles(int filesNo, int memNo);
+	//프로필 이미지 첨부파일 list(profile_img_view 조회)
+	List<FilesDto> profileImgList(int memNo);
 }
