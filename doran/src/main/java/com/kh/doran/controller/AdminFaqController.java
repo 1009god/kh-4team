c060eb2b789845851f269883719dc89bf7abe826
1009god3 <1009god3@naver.com>
2022-10-28 21:18:33 +0900

    faq(작성,수정,삭제),관리자 승인,회원,판매자 리스트 구현 완료

--- doran/src/main/java/com/kh/doran/controller/AdminFaqController.java
+++ doran/src/main/java/com/kh/doran/controller/AdminFaqController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kh.doran.entity.FaqDto;
+import com.kh.doran.error.TargetNotFoundException;
 import com.kh.doran.repository.FaqDao;
 
 @Controller
@@ -20,8 +22,7 @@ public class AdminFaqController {
 	private FaqDao faqDao;
 
 	@GetMapping("/faqlist")
-	public String list(Model model, 
-			@RequestParam(required = false) String type,
+	public String list(Model model, @RequestParam(required = false) String type,
 			@RequestParam(required = false) String keyword) {
 		boolean isSearch = type != null && keyword != null;
 		if (isSearch) {// 검색
@@ -31,30 +32,62 @@ public class AdminFaqController {
 		}
 		return "admin/faqlist";
 	}
-	
+
 	@GetMapping("/faqdetail")
-	public String detail(Model model,@RequestParam int faqNo) {
+	public String detail(Model model, @RequestParam int faqNo) {
 		FaqDto faqDto = faqDao.selectOne(faqNo);
-		model.addAttribute("faqDto",faqDto);
-		
+		model.addAttribute("faqDto", faqDto);
+
 		return "admin/faqdetail";
-		
+
 	}
-	
+
 	@GetMapping("/faqwrite")
 	public String insert() {
 		return "admin/faqwrite";
 	}
+
 	@PostMapping("/faqwrite")
 	public String insert(@ModelAttribute FaqDto faqDto) {
 		faqDao.insert(faqDto);
 		return "redirect:write_success";
 	}
-	
+
 	@GetMapping("/write_success")
 	public String insertSuccess() {
 		return "admin/faqwriteSuccess";
 	}
-	
-	
+
+	@GetMapping("/faqdelete")
+	public String faqdelete(@RequestParam int faqNo) {
+		boolean result = faqDao.delete(faqNo);
+		if (result) {
+			return "redirect:faqlist";
+		} else {
+			return "admin/editFail";
+		}
+	}
+
+	@GetMapping("/faqedit")
+	public String edit(Model model, @RequestParam int faqNo) {
+		FaqDto faqDto =faqDao.selectOne(faqNo);
+		if(faqDto == null) {
+			throw new TargetNotFoundException();
+		}
+		model.addAttribute("faqDto", faqDao.selectOne(faqNo));
+		return "admin/faqedit";
+	}
+
+	@PostMapping("/faqedit")
+	public String edit(@ModelAttribute FaqDto faqDto, RedirectAttributes attr) {
+		boolean result = faqDao.update(faqDto);
+		if (result) {
+			attr.addAttribute("faqNo", faqDto.getFaqNo());
+			return "redirect:faqdetail";
+		} else {
+			throw new TargetNotFoundException();
+		}
+	}
+
+
 }
