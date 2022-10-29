80c5473b448bef2f8d4c532c71c8bbcd3575a91a
scotty1003 <scotty1003@naver.com>
2022-10-29 14:45:29 +0900

    게시판 첨부파일 적용 중

--- doran/src/main/java/com/kh/doran/controller/NoticeController.java
+++ doran/src/main/java/com/kh/doran/controller/NoticeController.java
@@ -1,9 +1,13 @@
 package com.kh.doran.controller;
 
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +18,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.kh.doran.entity.FilesDto;
 import com.kh.doran.entity.NoticeDto;
 import com.kh.doran.error.TargetNotFoundException;
+import com.kh.doran.repository.FilesDao;
 import com.kh.doran.repository.NoticeDao;
 import com.kh.doran.vo.NoticeListSearchVO;
 
@@ -28,6 +35,16 @@ public class NoticeController {
 	
 	@Autowired
 	private NoticeDao noticeDao;
+	
+	@Autowired
+	private FilesDao filesDao; 
+	
+	private final File directory = new File(System.getProperty("user.home"), "doranupload");
+	
+	@PostConstruct //최초 실행시 딱 한 번만 실행되는 메소드
+	public void prepare() {
+		directory.mkdirs();
+	}
 	
 //	참고 : ModelAttribute로 수신한 데이터는 자동으로 Model에 첨부된다
 //	- 옵션에 name을 작성하면 해당하는 이름으로 model에 첨부
@@ -58,7 +75,8 @@ public class NoticeController {
 	@PostMapping("/write") 
 	public String write(
 			@ModelAttribute NoticeDto noticeDto,
-			HttpSession session, RedirectAttributes attr) {
+			@RequestParam List<MultipartFile> files,
+			HttpSession session, RedirectAttributes attr) throws IllegalStateException, IOException {
 		//session 에 있는 회원 번호를 작성자로 추가한 뒤 등록해야 함
 		int adminNo = (int)session.getAttribute("loginNo");
 		noticeDto.setNoticeAdminNo(adminNo);
@@ -69,6 +87,27 @@ public class NoticeController {
 		//문제점 : 등록은 되는데 몇 번인지 알 수 없다
 		//해결책 : 번호를 미리 생성하고 등록하도록 메소드 변경
 		int noticeNo = noticeDao.insert2(noticeDto);
+		
+		
+		//(+ 추가) 게시글이 등록된 다음 파일이 있다면 해당 파일을 등록 및 연결
+		//- 첨부파일이 없어도 리스트에는 1개의 객체가 들어있다
+		for(MultipartFile file : files) {
+			if(!file.isEmpty()) {
+				System.out.println("첨부파일 발견");
+				
+				int filesNo = filesDao.sequence();
+				filesDao.insert(FilesDto.builder()
+						.filesNo(filesNo)
+						.filesUploadname(file.getOriginalFilename())
+						.filesType(file.getContentType())
+						.filesSize(file.getSize())
+						.build());
+				//파일저장
+			
+				File target = new File(directory,String.valueOf(filesNo));
+				file.transferTo(target);
+			}
+		}
 		attr.addAttribute("noticeNo", noticeNo);
 		return "redirect:detail";
 	}
